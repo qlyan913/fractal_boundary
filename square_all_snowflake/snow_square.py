8176e2532d4883a9c1f8a8b567f0b10a5b59b06a
qlyan913 <yanqile1993@outlook.com>
2023-10-19 16:54:36 -0500

    update

--- square_all_snowflake/snow_square.py
+++ square_all_snowflake/snow_square.py
@@ -3,38 +3,42 @@ import numpy as np
 n=int(input("Enter the number of refinement steps for the pre-fractal upper boundary: "))
 mesh_size=input("Enter the meshsize: ")
 # Define the Koch snowflake vertices
-def koch_snowflake(vertices, level):
-    # input
-    #   vertices - real array of shape n x 2 (list of vertices)
-    #   level - non-negative integer (number of steps of Koch snowflake generation
-    # return
-    # array of generated vertices  (m x 2)
-    if level == 0:
-        return vertices
-    Rot=np.array([[0, -1],[1, 0]]) # rotation matrix
-    new_vertices = np.array([-1,-1]) # initiate the array
-    for i in range(len(vertices) - 1):
-        xl = vertices[i]
-        xr = vertices[i + 1]
-        # Calculate the new points
-        dx = xr - xl
-        ndx = np.linalg.norm(dx)
-        x1 = xl + dx/3.
-        x2 = x1 + np.matmul(Rot,dx/3.) # rotate the vector 60 degree
-        x3 = x2 + dx/3.
-        x4 = x1 + dx/3.
-        new_vertices = np.vstack([new_vertices, xl, x1,x2,x3,x4])
-    # delete first row
-    new_vertices = np.delete(new_vertices,0,0)
-    new_vertices = np.vstack([new_vertices, vertices[-1]])  # Add the last point
-    return koch_snowflake(new_vertices, level - 1)
+def koch_snowflake(vertices,new_vertices, level):
+   # input
+   #   vertices - real array of shape n x 2 (list of vertices)
+   #   level - non-negative integer (number of steps of Koch snowflake generation
+   # return
+   # array of generated vertices  (m x 2)
+   if level == 0:
+      return vertices
+   Rot=np.array([[0, -1],[1, 0]]) # rotation matrix
+   all_vertices = np.array([-1,-1]) # initiate the array
+   vertices_d = np.array([-1,-1]) # initiate the array 
+   for i in range(len(new_vertices) - 1):
+      xl = vertices[i]
+      xr = vertices[i + 1]
+      # Calculate the new points
+      dx = xr - xl
+      ndx = np.linalg.norm(dx)
+      x1 = xl + dx/3.
+      x2 = x1 + np.matmul(Rot,dx/3.) # rotate the vector 60 degree
+      x3 = x2 + dx/3.
+      x4 = x1 + dx/3.
+      all_vertices = np.vstack([all_vertices,xl, x1,x2,x3,x4])
+      vertices_d = np.vstack([vertices_d, x1,x2,x3,x4])
+
+   # delete first row
+   all_vertices = np.delete(all_vertices,0,0)
+   vertices_d = np.delete(vertices_d,0,0)
+   all_vertices = np.vstack([all_vertices, vertices[-1]])  # Add the last point
+   return koch_snowflake(all_vertices, vertices_d, level - 1)
 
 # Parameters
 #n =  4  # Number of iterations for Koch snowflake
 #mesh_size =0.02# Mesh size
 mesh_size2=mesh_size
-# Define the vertices for the Koch snowflake on top 
-vertices_koch = koch_snowflake(np.array([[0, 1], [1, 1]]), n)
+# Define the vertices for the Koch snowflake on top
+vertices_koch = koch_snowflake(np.array([[0, 1], [1, 1]]),np.array([[0, 1], [1, 1]]),n)
 
 # Generate the Gmsh script
 gmsh_script = f"""
