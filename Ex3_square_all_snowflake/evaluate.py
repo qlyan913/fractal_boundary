ede76d05f90d461c0f321e79e1d91aaf37dd3bd3
Qile Yan <37392192+qlyan913@users.noreply.github.com>
2023-10-26 17:47:39 -0500

    Add files via upload

--- Ex3_square_all_snowflake/evaluate.py
+++ Ex3_square_all_snowflake/evaluate.py
@@ -8,9 +8,24 @@ with CheckpointFile("solution.h5",'r') as afile:
 plt.figure()
 ii=[1,2,3,4,5]
 uu=[]
+pp1=[]
+pp2=[]
 for i in range(0,5):
    uu.append(uh.at([0.5,0.5*(3./2.)*(1-(1./3.)**(i+1))]))
+   pp1.append([0.5,0.5*(3./2.)*(1-(1./3.)**(i+1))])
+   pp2.append([0.5*(3./2.)*(1-(1./3.)**(i+1)),0.5])
 plt.plot(ii,uu,marker='o')
 plt.ylabel('evaluation of solution')
 plt.xlabel('$i$-th point')
 plt.savefig("evaluate.png")
+
+print("evaluation of solution  at points:")
+print(pp1)
+print("value:")
+print(uh.at(pp1))
+
+print("evaluation of solution  at points: ")
+print(pp2)
+print("value:")
+print(uh.at(pp2))
+
