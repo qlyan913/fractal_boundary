ce8cc8ddc53f61485bb839d66b599e81fe124bc8
Qile Yan <37392192+qlyan913@users.noreply.github.com>
2023-10-27 11:07:20 -0500

    Add files via upload

--- Ex3_square_all_snowflake/evaluate.py
+++ Ex3_square_all_snowflake/evaluate.py
@@ -8,23 +8,25 @@ with CheckpointFile("solution.h5",'r') as afile:
 plt.figure()
 ii=[1,2,3,4,5]
 uu=[]
+x_list=[]
 pp1=[]
 pp2=[]
 for i in range(0,5):
    uu.append(uh.at([0.5,0.5*(3./2.)*(1-(1./3.)**(i+1))]))
+   x_list.append(0.5*(3./2.)*(1-(1./3.)**(i+1)))
    pp1.append([0.5,0.5*(3./2.)*(1-(1./3.)**(i+1))])
    pp2.append([0.5*(3./2.)*(1-(1./3.)**(i+1)),0.5])
-plt.plot(ii,uu,marker='o')
+plt.plot(x_list,uu,marker='o')
 plt.ylabel('evaluation of solution')
-plt.xlabel('$i$-th point')
+plt.xlabel('position of $x$')
 plt.savefig("evaluate.png")
 plt.close()
 
 plt.figure()
-plt.semilogy(ii,uu,marker='o')
+plt.loglog(x_list,uu,marker='o')
 plt.ylabel('evaluation of solution')
-plt.xlabel('$i$-th point')
-plt.savefig("evaluate_logy.png")
+plt.xlabel('position of $x$')
+plt.savefig("evaluate_log.png")
 plt.close()
 
 
