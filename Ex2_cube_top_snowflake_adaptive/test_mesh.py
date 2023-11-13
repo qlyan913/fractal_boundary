2d30f6995eaedc519dae6a994f60a755637b7291
Qile <yanqile1993@outlook.com>
2023-11-13 13:36:36 -0600

    update

--- Ex2_cube_top_snowflake_adaptive/test_mesh.py
+++ Ex2_cube_top_snowflake_adaptive/test_mesh.py
@@ -3,7 +3,7 @@ from firedrake import *
 from firedrake.petsc import PETSc
 import numpy as np
 import time
-from netgen.geom2d import SplineGeometry
+from netgen.occ import *
 from geogen import *
 from Ex2_solver import *
 n=int(input("Enter the number of iterations for the pre-fractal boundary: "))
@@ -15,8 +15,17 @@ geo = MakeGeometry(n)
 ngmsh = geo.GenerateMesh(maxh=mesh_size)
 mesh0 = Mesh(ngmsh)
 
-# Plot the mesh
+# Save the mesh
 PETSc.Sys.Print(f'Finite element mesh has {mesh0.num_cells()} cells and {mesh0.num_vertices()} vertices.')
-outfile = File(f"domain/cube_{n}.pvd")
-outfile.write(mesh0)
+# Plot the mesh
+fig=plt.figure()
+ax = plt.axes(projection='3d')
+triplot(mesh0,axes=ax)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+plt.title('Koch Snowflake Mesh on Cube')
+plt.savefig(f"domain/cube_{n}.pdf")
+
+File(f"domain/cube_{n}.pvd").write(mesh0)
 PETSc.Sys.Print(f"File for visualization in Paraview saved as 'domain/cube_{n}.pvd.")
