cd6e431d607af77fb3d09cbd43cc2d6634e20378
Qile <yanqile1993@outlook.com>
2023-12-04 16:09:15 -0600

    update

--- Ex1_square_top_snowflake/main_flux.py
+++ Ex1_square_top_snowflake/main_flux.py
@@ -28,7 +28,7 @@ u = TrialFunction(V)
 v = TestFunction(V)
 
 # get flux Phi0 at lambda = 0
-Lambda=10**(-7)
+Lambda=10**(-8)
 a = Constant(D)*dot(grad(u), grad(v))*dx++Constant(D)/Constant(Lambda)*u*v*ds(4)
 L = Constant(0)*v*dx
 # list of boundary  ids that corresponds to the exterior boundary of the domain
@@ -43,7 +43,8 @@ print("phi0 is", Phi0)
 Phi=[]
 cc=[]
 import numpy as np
-LL = np.array([0.00001,0.001,0.01,0.1,0.2,0.5,1,1.5,2,2.5,5,10,15,20,40,60,100,200,400,600,1000])
+#LL = np.array([10**(-5),0.0001,0.001,0.01,0.1,0.2,0.5,1,1.5,2,2.5,5,10,15,20,40,60,100,200,400,600,1000])
+LL = np.array([2**i for i in range(-10,10)])
 for Lambda in LL:
   a = Constant(D)*dot(grad(u), grad(v))*dx+Constant(D)/Constant(Lambda)*u*v*ds(4)
   L = Constant(0)*v*dx
@@ -72,7 +73,7 @@ phi_2_log=np.log(phi_2)
 #c=exp(res.intercept)
 alpha=1
 plt.loglog(LL, phi_2,marker='o')
-plt.loglog(LL,(LL)**alpha/(LL[-1]**alpha)*(phi_2[-1]),marker='o')
+plt.loglog(LL,(LL)**alpha/(LL[0]**alpha)*(phi_2[0]),marker='o')
 plt.legend(['$1/\Phi-1/\Phi_0$', '$~\Lambda^{{%s}}$' % (alpha)])
 plt.xlabel('$\Lambda$')
 plt.savefig(f"figures/Phi_Lam_{nn}.png")
@@ -89,7 +90,7 @@ PETSc.Sys.Print(f"Result for 0<Lambda<1000 saved to results/Phi_Lam_{nn}.csv ")
 # Region 1: Lambda <1 
 Phi=[]
 cc=[]
-LL = np.array([10**(-4),0.001,0.002,0.005,0.01,0.02,0.05,0.08,0.1,0.2,0.4,0.8,1])
+LL = np.array([10**(-5),10**(-4),0.001,0.002,0.005,0.01,0.02,0.05,0.08,0.1,0.2,0.4,0.8,1])
 for Lambda in LL:
   a = Constant(D)*dot(grad(u), grad(v))*dx+Constant(D)/Constant(Lambda)*u*v*ds(4)
   L = Constant(0)*v*dx
