113637e7aee09d60a4ee1c5698a26caa956b1b6b
Qile <yanqile1993@outlook.com>
2023-12-09 17:40:30 -0600

    update

--- Ex1_square_top_snowflake_adaptive/main_flux.py
+++ Ex1_square_top_snowflake_adaptive/main_flux.py
@@ -69,15 +69,15 @@ for i in range(len(Phi)):
    phi_2.append(1/Phi[i]-1/Phi0)
 phi_2_log=np.log(phi_2)
 alpha=1/dim_frac
-plt.loglog(LL, phi_2,marker='o',color='blue')
-plt.loglog(LL,(LL)**alpha/(LL[-1]**alpha)*(phi_2[-1]),marker='o',color='red',linestyle='dashed')
+plt.loglog(LL[0:-2], phi_2[0:-2],marker='o',color='blue')
+plt.loglog(LL,(LL)**alpha/(LL[-1]**alpha)*(phi_2[-1]),color='red',linestyle='dashed')
 alpha=1
-plt.loglog(LL,(LL)**alpha/(LL[0]**alpha)*(phi_2[0]),marker='o',color='black',linestyle='dashed')
-plt.loglog(LL,(LL)**alpha/(LL[-3]**alpha)*(phi_2[-3]),marker='o',color='black',linestyle='dashed')
+plt.loglog(LL,(LL)**alpha/(LL[0]**alpha)*(phi_2[0]),color='black',linestyle='dashed')
+plt.loglog(LL,(LL)**alpha/(LL[-3]**alpha)*(phi_2[-3]),color='black',linestyle='dashed')
 plt.axvline(x=l,color='cyan',linestyle='dashed')
 plt.axvline(x=Lp,color='cyan',linestyle='dashed')
 plt.legend(['$1/\Phi-1/\Phi_0$','$O(\Lambda^{1/dim_frac})$', '$O(\Lambda^{1})$'])
-plt.xticks([l, Lp], ['l', 'Lp'])
+plt.xticks([10**(-4),10**(-3),10**(-2),10**(-1),1,10**(1),10**(2),10**3,l, Lp], ['$10^{-4}$','$10^{-3}$','$10^{-2}$','$10^{-1}$','$10^{0}$','$10^{1}$','$10^{2}$','$10^{3}$','l', 'Lp'])
 plt.xlabel('$\Lambda$')
 plt.savefig(f"figures/Phi_Lam_{nn}.png")
 PETSc.Sys.Print(f"Plot for 0<Lambda<1000 saved to figures/Phi_Lam_{nn}.png ")
@@ -100,7 +100,7 @@ for i in range(len(Phi)):
    phi_2.append(1/Phi[i]-1/Phi0)
 alpha=1
 plt.loglog(LL, phi_2,marker='o',color='blue')
-plt.loglog(LL,(LL)**alpha/(LL[-1]**alpha)*(phi_2[-1]),marker='o',color='black',linestyle='dashed')
+plt.loglog(LL,(LL)**alpha/(LL[-1]**alpha)*(phi_2[-1]),color='black',linestyle='dashed')
 plt.legend(['$1/\Phi-1/\Phi_0$', '$O(\Lambda^{1})$'])
 plt.xlabel('$\Lambda$')
 plt.savefig(f"figures/Phi_Lam_{nn}_R1.png")
@@ -126,7 +126,7 @@ for i in range(len(Phi)):
    phi_2.append(1/Phi[i]-1/Phi0)
 alpha=1/dim_frac
 plt.loglog(LL, phi_2,marker='o',color='blue')
-plt.loglog(LL,(LL)**alpha/(LL[0]**alpha)*(phi_2[0]),marker='o',color='red',linestyle='dashed')
+plt.loglog(LL,(LL)**alpha/(LL[0]**alpha)*(phi_2[0]),color='red',linestyle='dashed')
 plt.legend(['$1/\Phi-1/\Phi_0$', '$O(\Lambda^{1/dim_frac})$'])
 plt.xlabel('$\Lambda$')
 plt.savefig(f"figures/Phi_Lam_{nn}_R2.png")
@@ -150,7 +150,7 @@ for i in range(len(Phi)):
    phi_2.append(1/Phi[i]-1/Phi0)
 alpha=1
 plt.loglog(LL, phi_2,marker='o',color='blue')
-plt.loglog(LL,(LL)**alpha/(LL[0]**alpha)*(phi_2[0]),marker='o',color='black',linestyle='dashed')
+plt.loglog(LL,(LL)**alpha/(LL[0]**alpha)*(phi_2[0]),color='black',linestyle='dashed')
 plt.legend(['$1/\Phi-1/\Phi_0$', '$O(\Lambda^{1})$'])
 plt.xlabel('$\Lambda$')
 plt.savefig(f"figures/Phi_Lam_{nn}_R3.png")
