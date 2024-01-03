2ebb3f594e0bade90e1c35b60dab7fde06525a77
qlyan913 <yanqile1993@outlook.com>
2024-01-03 08:44:08 -0800

    update

--- Ex2_cube_top_snowflake_v3/main_flux_R1.py
+++ Ex2_cube_top_snowflake_v3/main_flux_R1.py
@@ -99,33 +99,27 @@ Lambda=10**(-11)
 Phi =get_flux(ngmsh,[Lambda],nn,deg,tolerance,max_iterations,bc_left,bc_right,bc_front,bc_back,bc_bot,bc_top)
 Phi0=Phi[0]
 PETSc.Sys.Print('Phi0 is ', Phi0)
-# calculate flux for various Lambda 
-LL = np.array([2**i for i in range(-15,10)])
-LL=np.append(LL,[l,Lp])
+# Region 1: 0<Lambda <l 
+LL = np.array([4**(-i) for i in range(nn,10)])
 Phi =get_flux(ngmsh,LL,nn,deg,tolerance,max_iterations,bc_left,bc_right,bc_front,bc_back,bc_bot,bc_top)
 fig, axes = plt.subplots()
 phi_2=[]
 for i in range(len(Phi)):
    phi_2.append(1/Phi[i]-1/Phi0)
-phi_2_log=np.log(phi_2)
-alpha=1/dim_frac
-plt.loglog(LL[0:-2], phi_2[0:-2],marker='o',color='blue')
-plt.loglog(LL,(LL)**alpha/(LL[-1]**alpha)*(phi_2[-1]),color='red',linestyle='dashed',linewidth=0.8)
 alpha=1
-plt.loglog(LL,(LL)**alpha/(LL[0]**alpha)*(phi_2[0]),color='black',linestyle='dashed',linewidth=0.8)
-plt.loglog(LL,(LL)**alpha/(LL[-3]**alpha)*(phi_2[-3]),color='black',linestyle='dashed',linewidth=0.8)
+plt.loglog(LL, phi_2,marker='o',color='blue')
+plt.loglog(LL,(LL)**alpha/(LL[-1]**alpha)*(phi_2[-1]),color='black',linestyle='dashed',linewidth=0.8)
 plt.axvline(x=l,color='cyan',linestyle='dashed')
-plt.axvline(x=Lp,color='cyan',linestyle='dashed')
-plt.legend(['$1/\Phi-1/\Phi_0$','$O(\Lambda^{1/dim_frac})$', '$O(\Lambda^{1})$'])
-plt.xticks([10**(-4),10**(-3),10**(-2),10**(-1),1,10**(1),10**(2),10**3,l, Lp], ['$10^{-4}$','$10^{-3}$','$10^{-2}$','$10^{-1}$','$10^{0}$','$10^{1}$','$10^{2}$','$10^{3}$','l', 'Lp'])
+plt.xticks([10**(-7),10**(-6),10**(-5),10**(-4),10**(-3),l],['$10^{-7}$','$10^{-6}$','$10^{-5}$','$10^{-4}$','$10^{-3}$','l'])
+plt.legend(['$1/\Phi-1/\Phi_0$', '$O(\Lambda^{1})$'])
 plt.xlabel('$\Lambda$')
-plt.savefig(f"figures/Phi_Lam_{nn}.png")
-PETSc.Sys.Print(f"Plot for 0<Lambda<1000 saved to figures/Phi_Lam_{nn}.png ")
-with open(f'results/Phi_Lam_{nn}.csv', 'w', newline='') as csvfile:
+plt.savefig(f"figures/Phi_Lam_{nn}_R1.png")
+PETSc.Sys.Print(f"plot for 0<Lambda<l saved to figures/Phi_Lam_{nn}_R1.png ")
+with open(f'results/Phi_Lam_{nn}_R1.csv', 'w', newline='') as csvfile:
     fieldnames = ['Lambda', 'flux']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     writer.writerow({'Lambda': 0, 'flux': Phi0})
     for i in range(len(LL)):
-       writer.writerow({'Lambda': LL[i], 'flux': Phi[i]})
-PETSc.Sys.Print(f"Result for 0<Lambda<1000 saved to results/Phi_Lam_{nn}.csv ")
+       writer.writerow({'Lambda': LL[i], 'flux': Phi[i] })
+PETSc.Sys.Print(f"Result for 0<Lambda<l saved to results/Phi_Lam_{nn}_R1.csv ")
