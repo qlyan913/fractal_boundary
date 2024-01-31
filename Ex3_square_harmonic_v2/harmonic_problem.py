2e00c46f136722cb607ae5cc02270e24532baa1e
Qile <yanqile1993@outlook.com>
2024-01-30 18:25:58 -0600

    update

--- Ex3_square_harmonic_v2/harmonic_problem.py
+++ Ex3_square_harmonic_v2/harmonic_problem.py
@@ -46,9 +46,9 @@ from firedrake.pyplot import tripcolor
 fig, axes = plt.subplots()
 collection = tripcolor(uh, axes=axes)
 fig.colorbar(collection);
-plt.savefig(f"figures/solution_{n}.png")
+plt.savefig(f"figures/solution_{n}.pdf")
 plt.close()
-PETSc.Sys.Print(f"The plot of solution is saved to figures/solution_{n}.png")
+PETSc.Sys.Print(f"The plot of solution is saved to figures/solution_{n}.pdf")
 
 uu=uh.at(pp)
 plt.figure()
