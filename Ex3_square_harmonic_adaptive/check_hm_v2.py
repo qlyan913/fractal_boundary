a433a1331b159534862d7d74c08fa492c477c8db
Qile <yanqile1993@outlook.com>
2024-03-28 13:27:10 -0500

    update

--- Ex3_square_harmonic_adaptive/check_hm_v2.py
+++ Ex3_square_harmonic_adaptive/check_hm_v2.py
@@ -72,7 +72,7 @@ for N in N_all:
      if alpha_list[i]>alpha_0:
         ms=ms+c_list[i]*(l/2.)**alpha_list[i]
         ms_u=ms_u+uh.at(xl_list[i])
-     if nf<5 and std_list[i]>0.8*max(std_list):
+     if nf<5 and std_list[i]>0.5*max(std_list):
         plot_regression(f"reg_figs/n{n}/reg_n{n}_N{N}_i{i}.png",uu_all_list[i],c_list[i],dy_list,alpha_list[i],uh.at(xl_list[i]),l/2.)
         nf=nf+1
    ms_list.append(ms)
